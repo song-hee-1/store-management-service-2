12d9e9a75ec39af4cf985a65c78910865b03c984
한송희 <98_han@naver.com>
2022-10-22 10:16:02 +0900

    Feat : custom RegisterSerializer 및 adapters 생성
    - dj_rest_auth 회원가입 custom을 위하여 CustomRegisterSerializer 생성
    - 유저 생성시 추가 필드 저장을 위하여 all_auth의CustomAdapters 생성

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
+from dj_rest_auth.registration.serializers import RegisterSerializer
 
 from apps.accounts.models import User
+
+from django.core.validators import RegexValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    phoneNumberRegex = RegexValidator(regex=r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    phone_number = serializers.CharField(validators=[phoneNumberRegex],
+                                         help_text="휴대폰 번호는 다음과 같은 형식을 따라야 합니다: 010-1234-5678")
+    date_of_birth = serializers.DateField()
+
+    def save(self, request):
+        user = super().save(request)
+        user.phone_number = self.data.get('phone_number')
+        user.date_of_birth = self.data.get('date_of_birth')
+        user.save()
+        return user
