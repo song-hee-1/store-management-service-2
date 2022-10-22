40b350d91722fff997b6b778e7c6448af4d9652f
한송희 <98_han@naver.com>
2022-10-22 22:46:20 +0900

    Feat : product 모델링에 price 필드 추가
    - price가 누락되어 필드 추가 정의

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# class Order(models.Model):
+#     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE, db_column='user_id', related_name='')
+#     product = models.ForeignKey("proudcts.Product", on_delete=models.CASCADE, db_column='product_id', related_name='')
+#     address = models.
+#     receiver_name
+#     order_state
+#     order_date
+#     total_price
--- apps/products/models.py
+++ apps/products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=20, verbose_name="상품이름")
     description = models.TextField(verbose_name="상품설명")
+    price = models.PositiveIntegerField(verbose_name="상품가격", default=0)
     stock = models.PositiveIntegerField(verbose_name="재고")
     create_at = models.DateTimeField(auto_now=True, verbose_name="상품 업로드 날짜")
 
