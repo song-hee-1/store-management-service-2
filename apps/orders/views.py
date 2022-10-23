159f7815cbf7a07de2d6d2d18ea8f7af53d2044b
한송희 <98_han@naver.com>
2022-10-23 14:14:32 +0900

    Feat : 주문서 삭제 불가능하도록 변경

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
@@ -7,7 +9,13 @@ from apps.products.models import Product
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        q = self.request.GET.get('order_state', '')
+        if q:
+            queryset = queryset.filter(Q(order_state__iexact=q))
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -29,12 +37,17 @@ class OrderViewSet(viewsets.ModelViewSet):
                 product_id = product.id
                 total_price = Product.objects.get(id=product_id).price
             except Exception as e:
-                message = {f"ERROR: 에러가 발생하였습니다. {e}"}
+                message = {'ERROR': f'에러가 발생하였습니다. {e}'}
                 return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
         self.perform_create(serializer, total_price)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, pk=None):
+        """ DELETE 메소드는 허용하지 않습니다. """
+        response = {'ERROR': 'DELETE 메소드는 허용하지 않습니다.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def perform_create(self, serializer, total_price=None):
         serializer.save(total_price=total_price)
