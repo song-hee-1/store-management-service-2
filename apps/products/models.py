773f74334dd850883f6b0241be52bde25d0afa08
한송희 <98_han@naver.com>
2022-10-23 10:42:11 +0900

    Feat : order 모델링
    - user와 product는 M:N 관계로 orders라는 중간테이블을 가짐
    - 수령자 정보, 주문서 상태 등의 추가적인 정보 저장을 위하여 through로 중간테이블 직접 지정

--- apps/products/models.py
+++ apps/products/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=20, verbose_name="상품이름")
-    description = models.TextField(verbose_name="상품설명")
-    price = models.PositiveIntegerField(verbose_name="상품가격", default=0)
+    name = models.CharField(max_length=20, verbose_name="상품 이름")
+    description = models.TextField(verbose_name="상품 설명")
+    price = models.PositiveIntegerField(verbose_name="상품 가격", default=0)
     stock = models.PositiveIntegerField(verbose_name="재고")
     create_at = models.DateTimeField(auto_now=True, verbose_name="상품 업로드 날짜")
+    order = models.ManyToManyField(
+        "accounts.User",
+        through="orders.Order",
+        related_name='user_order',
+        blank=True,
+        verbose_name="상품 주문내역"
+    )
 
     class Meta:
         verbose_name = "상품"
