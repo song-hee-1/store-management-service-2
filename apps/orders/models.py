e56e52b70742bdd87ed5899c4cf1d5bb9f26e181
한송희 <98_han@naver.com>
2022-10-23 14:08:02 +0900

    Fix : order_state 저장 형태 수정
    - default가 int로 잘못 설정되어 있어 필터링 불가
    - 올바른 형태로 저장되도록 수정
    - 결제에서 주문으로 값 수정

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -4,8 +4,8 @@ from django.core.validators import RegexValidator
 
 class Order(models.Model):
     ORDER_STATE_CHOICES = (
-        ('결제취소', 0),
-        ('결제완료', 1),
+        ('주문취소', 0),
+        ('주문완료', 1),
     )
 
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE, db_column='user_id',
@@ -17,7 +17,7 @@ class Order(models.Model):
     phoneNumberRegex = RegexValidator(regex=r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
     receiver_phone_number = models.CharField(validators=[phoneNumberRegex], max_length=13,
                                              help_text="휴대폰 번호는 다음과 같은 형식을 따라야 합니다: 010-1234-5678")
-    order_state = models.CharField(max_length=4, default=1, choices=ORDER_STATE_CHOICES, verbose_name="주문서 상태")
+    order_state = models.CharField(max_length=4, default="주문완료", choices=ORDER_STATE_CHOICES, verbose_name="주문서 상태")
     order_date = models.DateTimeField(auto_now_add=True, verbose_name="주문 날짜")
     total_price = models.PositiveIntegerField(null=True, verbose_name="주문서 총 가격")
 
