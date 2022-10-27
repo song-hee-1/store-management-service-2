879085ffca44dccc9c0c61617ebadbf8d128d321
한송희 <98_han@naver.com>
2022-10-27 12:10:26 +0900

    Feat : cart 모델링
    - 여러 상품을 장바구에 넣을수 있도록 cart와 cartitem 분리하여 모델링
    - 설정오류로 migrations 파일 삭제 후 다시 migrations 적용

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name="cart", null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "장바구니"
+        verbose_name_plural = "장바구니 목록"
+        db_table = 'cart'
+
+    def __str__(self):
+        return self.user
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey("cart.Cart", on_delete=models.CASCADE, related_name="items", null=True)
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE, null=True,
+                                related_name='cartitems')
+    quantity = models.PositiveIntegerField(default=0)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "장바구니 상품"
+        verbose_name_plural = "장바구니 상품 목록"
+        db_table = 'cartitem'
+
+    def __str__(self):
+        return {self.cart, self.product}
