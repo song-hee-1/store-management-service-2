c398c994450ccc960345adc337e30120e1cf0a00
한송희 <98_han@naver.com>
2022-10-27 23:37:26 +0900

    Refactor : user url 및 password 속성 변경
    - password는 보이지 않게 write only로 설정

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -10,6 +10,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 
 class CustomRegisterSerializer(RegisterSerializer):
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from apps.accounts.models import User
 from apps.accounts.serializers import UserSerializer
@@ -7,3 +8,8 @@ from apps.accounts.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        """ CREATE 메소드는 허용하지 않습니다. """
+        response = {'ERROR': 'CREATE 메소드는 허용하지 않습니다.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
