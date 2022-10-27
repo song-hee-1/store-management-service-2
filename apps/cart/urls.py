9565cf4d7fe9b2785cb121af719c434a05df70c0
한송희 <98_han@naver.com>
2022-10-27 14:38:02 +0900

    Feat : cart, cartitem views 및 urls 추가
    - viewset을 이용하여 cart, cartitem 개발 및 router 추가

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from apps.cart.views import CartViewSet, CartItemViewSet
+
+router = routers.DefaultRouter()
+router.register('carts', CartViewSet, basename='cart')
+router.register('items', CartItemViewSet, basename='cart-item')
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from apps.cart.models import Cart, CartItem
+from apps.cart.serializers import CartSerializer, CartItemSerializer
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
