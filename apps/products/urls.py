53814eddacf3cda1ca8235227139de8a066e3076
한송희 <98_han@naver.com>
2022-10-27 22:48:50 +0900

    Refactor : url 수정

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from apps.products.views import ProudctViewset
 
 router = routers.DefaultRouter()
-router.register('proudcts', ProudctViewset, basename='products')
+router.register('products', ProudctViewset, basename='products')
 
 urlpatterns = [
     path('', include(router.urls)),
--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path('accounts/', include('apps.accounts.urls')),
     path('orders/', include('apps.orders.urls')),
-    path('products/', include('apps.products.urls')),
+    path('', include('apps.products.urls')),
     path('', include('apps.cart.urls')),
 ]
